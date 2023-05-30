d0797ec526a64eabb3aa72738e42f8bf57db291e
AnaoGOD <goncalocgois@gmail.com>
2023-05-30 18:35:06 +0100

    Galeria completa (provavelmente)

--- PAP/javascript/galeria.js
+++ PAP/javascript/galeria.js
@@ -1,18 +1,20 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+//Controlo prev/next
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
 
+//Controlo slide imagem
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("transparente");
   let captionText = document.getElementById("caption");
   if (n > slides.length) {
     slideIndex = 1;
@@ -24,9 +26,10 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" ativo", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].className += " ativo";
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
